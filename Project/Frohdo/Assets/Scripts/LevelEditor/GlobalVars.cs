1e1243553784106fa0cbe3a891b5bdb996f0d4af
Tobias Pretzl <tpretzl.mmt-b2012@fh-salzburg.ac.at>
2014-04-06 04:14:39 +0200

    Work on Leveleditor: stated building GUI.

---
+++ Project/Frohdo/Assets/Scripts/LevelEditor/GlobalVars.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVars  {
+    public static int levelWidth = 30;
+    public static int levelHeight = 20;
+    public static int parallaxFrontWidth = 50;
+    public static int parallaxBackWidth = 50;
+}
---
+++ Project/Frohdo/Assets/Scripts/LevelEditor/Gui_Main.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gui_Main : MonoBehaviour {
+
+    public int leftAreaWidth;
+    public GUIStyle leftAreaStyle;
+
+    public Texture testTex;
+
+    private string levelName = "Enter level name";
+    
+    private int activeLayer = 1;
+    private string[] activeLayerStings;
+    private bool[] visibleLayer;
+    private Vector2 scrollPos;
+    private int selectedLevelobject = 0;
+    private GUIContent[] levelObjects_content;
+
+    // Use this for initialization
+	void Start () { 
+        activeLayerStings = new string[5];
+        visibleLayer = new bool[5];
+        scrollPos = new Vector2(0, 0);
+        for (int i = 0; i < 5; i++)
+        {
+            activeLayerStings[i] = (i+1).ToString();
+            visibleLayer[i] = true;
+        }
+        levelObjects_content = new GUIContent[100];
+        int texS = (leftAreaWidth - 24)/3;
+
+        for(int i=0; i<levelObjects_content.Length; i++){
+            levelObjects_content[i] = new GUIContent(testTex);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI ()
+    {
+        //left Area
+        GUILayout.BeginArea(new Rect(0,0,leftAreaWidth,Screen.height),leftAreaStyle);
+        levelName = GUILayout.TextField(levelName);
+        GUILayout.Label("ACTIVE LAYER");
+        activeLayer = GUILayout.Toolbar(activeLayer, activeLayerStings);
+        GUILayout.Label("VIVIBLE LAYER");
+        GUILayout.BeginHorizontal();
+            for (int i = 0; i < visibleLayer.Length; i++)
+            {
+                visibleLayer[i] = GUILayout.Toggle(visibleLayer[i], (i + 1).ToString());
+            }
+        GUILayout.EndHorizontal();
+        GUILayout.Label("LEVELOBJECT");
+        scrollPos = GUILayout.BeginScrollView(scrollPos);
+        GUILayout.SelectionGrid(selectedLevelobject, levelObjects_content, 2,GUILayout.Width(leftAreaWidth-24));
+        GUILayout.EndScrollView();
+        GUILayout.Button("PLAY");
+        GUILayout.EndArea();
+    }
+}
