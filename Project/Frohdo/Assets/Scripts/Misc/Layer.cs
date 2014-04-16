fe3670eec4a49a2a01645e64247bd9120d0f5fd7
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2014-04-16 02:18:48 +0200

    created game scene Implemented access from script to levelObject prefabs via the LevelObjectController (maybe needs to be singleton, needs discussion) also implemented adjustable parallax layers

---
+++ Project/Frohdo/Assets/Scripts/Misc/Layer.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Layer : MonoBehaviour {
+
+    //public
+    public LevelObjectController levelObjectController_;
+    public Camera camera_;
+
+    public Vector2 parallaxFactor_;
+    public bool hasColliders_;
+
+    //private
+    //private List<GameObject> levelObjects_;//not sure if i need this
+
+
+	// Use this for initialization
+	void Start () {
+	    //levelObjects_ = new List<GameObject>();
+
+        //float distanceToCamera = transform.position.z - camera_.transform.position.z;
+
+        for (int x = -6; x <= 6; x += 2)
+        {
+            for (int y = -6; y <= 6; y += 2)
+            {
+                AddLevelObjectByName("1x1Tile_Test", new Vector2(x, y));
+            }
+        }
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //adjust the position according to the parallaxFactor and the camera
+        Vector3 newPosition = transform.position;
+        newPosition.x = -parallaxFactor_.x * camera_.transform.position.x;
+        newPosition.y = -parallaxFactor_.y * camera_.transform.position.y;
+        transform.position = newPosition;
+
+        //test
+        //if(Input.GetButton("Jump"))
+        //{
+        //    AddLevelObjectByName("TestPrefab");
+        //}
+	}
+
+
+    public void AddLevelObjectByName(string prefabName, Vector2 position)
+    {
+        GameObject prefab;
+        try
+        {
+            prefab = levelObjectController_.GetPrefabByName(prefabName);
+        }
+        catch
+        {
+            Debug.Log("LevelObject prefab not added to Layer: " + prefabName);
+            return;
+        }
+
+        GameObject levelObject = (GameObject)Instantiate(prefab);
+        levelObject.name = prefabName;
+        levelObject.transform.parent = transform;
+        levelObject.transform.localPosition = position;
+        levelObject.GetComponent<Collider2D>().enabled = hasColliders_;
+
+        //levelObjects_.Add(levelObject);
+    }
+}
---
+++ Project/Frohdo/Assets/Scripts/Misc/LevelObjectController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelObjectController : MonoBehaviour {
+
+    //public
+    public List<GameObject> levelObjectPrefabs_;
+
+    public GameObject GetPrefabByName(string name)
+    {
+        foreach (GameObject levelObjectPrefab in levelObjectPrefabs_)
+        {
+            if (levelObjectPrefab.name.Equals(name))
+            {
+                return levelObjectPrefab;
+            }
+        }
+
+        Debug.Log("LevelObject prefab not found: " + name);
+        throw new System.Exception("LevelObject prefab not found: " + name);
+    }
+}
