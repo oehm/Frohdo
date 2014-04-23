e1691ff334a4efcb7bc174a190a5d7b171bf7b5d
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2014-04-24 00:21:41 +0200

    Made the LevelObjectController kind of a singleton. its members are editable in the unity editor through the ScriptableObjectInstance. Also added utility to create instances of scriptableObjects in the unity editor. Improved error checking in layer

---
+++ Project/Frohdo/Assets/Scripts/Utility/LevelObjectControllerAsset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class LevelObjectControllerAsset : MonoBehaviour {
+
+    [MenuItem("Assets/Create/LevelObjectController")]
+
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<LevelObjectController>();
+    }
+}
