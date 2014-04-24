bce014459a4d61a892f725df5dd10edc4b86b63a
Tobias Pretzl <tpretzl.mmt-b2012@fh-salzburg.ac.at>
2014-04-24 20:57:40 +0200

    Renamed Utility Folder to editor so unity doesn't compile those scripts when building. Forced the spect ratio to be 16:9 works good in Windowed mode but still some bug in fullscreen wich actually are unitys fault

---
+++ Project/Frohdo/Assets/Scripts/Misc/ForceAspectRatio.cs
@@ -0,0 +1,56 @@
+﻿// CameraAspectRatio
+// By Nicolas Varchavsky @ Interatica (www.interatica.com)
+// Date: March 27th, 2009
+// Version: 1.0
+
+//Translated to C#  and adaped by Tobias Pretzl
+
+
+using UnityEngine;
+using System.Collections;
+
+public class ForceAspectRatio : MonoBehaviour
+{
+
+    void Awake()
+    {
+        changeRatio();
+    }
+
+    public void changeRatio()
+    {
+        float sw = GlobalVars.curResX;
+        float sh = GlobalVars.curResY;
+
+
+        float th = sw * (GlobalVars.aspecY / GlobalVars.aspecX);
+
+        float ptw = 1.0f;
+        float pth = 1.0f;
+
+        float tx = 0.0f;
+        float ty = 0.0f;
+        float half = 0.0f;
+
+        pth = th / sh;
+
+        if (pth > 1.0f)
+        {
+            float tw = sh * (GlobalVars.aspecX / GlobalVars.aspecY);
+            ptw = tw / sw;
+
+            half = (1.0f - ptw) / 2.0f;
+
+            tx = half;
+        }
+        else
+        {
+            half = (1.0f - pth) / 2.0f;
+
+            ty = half;
+        }
+
+        Rect r = new Rect(tx, ty, ptw, pth);
+        camera.rect = r;
+    }
+}
--- Project/Frohdo/Assets/Scripts/Misc/GlobalVars.cs
+++ Project/Frohdo/Assets/Scripts/Misc/GlobalVars.cs
@@ -3,4 +3,10 @@ using System.Collections;
 
 public class GlobalVars  {
     public static int numberofLayers = 5;
+
+    public static float aspecX = 16.0f;
+    public static float aspecY = 9.0f;
+
+    public static int curResX = 1280;
+    public static int curResY = 720;
 }
