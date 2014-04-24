cd8ec34bba3bc54a0757e2916bde6788540a13bd
Tobias Pretzl <tpretzl.mmt-b2012@fh-salzburg.ac.at>
2014-04-24 22:46:29 +0200

    Added PauseMenu so u can actually go back to MainMenu from Game or LevelEditor

---
+++ Project/Frohdo/Assets/Scripts/Misc/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    private delegate void MenuDelegate();
+    private MenuDelegate menuFunction;
+
+    private bool isPause = false;
+    // Use this for initialization
+    void Start()
+    {
+        menuFunction = pauseMenuMain;
+    }
+
+    void Update()
+    {
+        isPause = Input.GetAxis("Pause") > 0.0f;
+    }
+    
+    void OnGUI()
+    {
+        if (isPause)
+        {
+            menuFunction();
+        }
+    }
+
+    void pauseMenuMain()
+    {
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - 300, Screen.height / 2 - 200, 600, 400));
+        if (GUILayout.Button("Back to Main Menu"))
+        {
+            Application.LoadLevel(1);
+        }
+
+        if (GUILayout.Button("Quit"))
+        {
+            Application.Quit();
+        }
+        GUILayout.EndArea();
+    }
+}
