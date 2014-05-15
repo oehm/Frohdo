496717bb35b8a584688ea3e9265a44131d00b504
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2014-05-15 19:26:56 +0200

    reworked scene manager, so you dont need the scene destroyer ref

--- Project/Frohdo/Assets/temp_data/SkipToMainMenu.cs
+++ Project/Frohdo/Assets/temp_data/SkipToMainMenu.cs
@@ -6,7 +6,7 @@ public class SkipToMainMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        SceneManager.Instance.loadScene(destroyer, 2);
+        SceneManager.Instance.loadScene(SceneManager.Scene.MainMenu);
 
 	}
 	
