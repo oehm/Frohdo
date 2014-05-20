67754ec9590204aa36645e447d3b7314d0d42b83
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2014-05-20 12:14:58 +0200

    reworked pickup mechanics (pick up 1 at a time); same for use;

--- Project/Frohdo/Assets/Scripts/Game/Character/CharacterPickup.cs
+++ Project/Frohdo/Assets/Scripts/Game/Character/CharacterPickup.cs
@@ -5,25 +5,36 @@ public class CharacterPickup : MonoBehaviour {
 
     private Character character_;
 
-    public bool EnabledUp { get; set; }
-    public bool EnabledDown { get; set; }
+    public bool EnabledUp { get{return enabledUp_;} set{enabledUp_ = value; if(value == false) disabledUp_ = false;} }
+    public bool EnabledDown { get{return enabledDown_;} set{enabledDown_ = value; if(value == false) disabledDown_ = false;} }
+    
+    private bool enabledUp_;
+    private bool enabledDown_;
 
+    private bool disabledUp_;
+    private bool disabledDown_;
 
 	// Use this for initialization
 	void Start () {
         character_ = gameObject.transform.parent.gameObject.GetComponentInChildren<Character>();
+
+        disabledUp_ = false;
+        disabledDown_ = false;
 	}
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (EnabledUp && coll.gameObject.GetComponentInChildren<Usable>() != null)
+        if (!disabledUp_ && EnabledUp && coll.gameObject.GetComponentInChildren<Usable>() != null)
         {
             character_.use(coll.gameObject);
+            disabledUp_ = true;
         }
 
-        if (EnabledDown && coll.gameObject.GetComponentInChildren<Collectable>() != null)
+        if (!disabledDown_ && EnabledDown && coll.gameObject.GetComponentInChildren<Collectable>() != null)
         {
             character_.pickUp(coll.gameObject);
+            disabledDown_ = true;
+
         }
     }
 }
