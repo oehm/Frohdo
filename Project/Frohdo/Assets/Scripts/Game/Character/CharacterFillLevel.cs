fda259c74090782a95a63d0f02c879e1757576ef
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2014-05-21 18:15:23 +0200

    puke level done

---
+++ Project/Frohdo/Assets/Scripts/Game/Character/CharacterFillLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFillLevel : MonoBehaviour {
+
+    public string textureName_;
+
+
+    private Character character_;
+    private CharacterPuke characterPuke_;
+    private Material material_;
+
+	// Use this for initialization
+	void Start () {
+        character_ = gameObject.GetComponentInChildren<Character>();
+        characterPuke_ = character_.gameObject.GetComponentInChildren<CharacterPuke>();
+        material_ = character_.gameObject.GetComponentInChildren<Renderer>().material;
+	}
+
+    void Update()
+    {
+        material_.SetTextureOffset(textureName_, new Vector2(0.0f, 0.5f - 0.25f * characterPuke_.Ratios));
+    }
+	
+}
