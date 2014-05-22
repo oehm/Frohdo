2e77fc51f6851910ecd8f4cfd9c5b4719c4e4a6f
Tobias Pretzl <tpretzl.mmt-b2012@fh-salzburg.ac.at>
2014-05-22 18:05:54 +0200

    fixed setting values in editor_grid with objects that don't have hit zones everywhere

--- Project/Frohdo/Assets/Scripts/LevelEditor/Command/CommandHelper.cs
+++ Project/Frohdo/Assets/Scripts/LevelEditor/Command/CommandHelper.cs
@@ -7,14 +7,14 @@ public class CommandHelper
     {
         int pW = Editor_Grid.Instance.levelGrid[matlayer].Length + 1;
         int pH = Editor_Grid.Instance.levelGrid[matlayer][0].Length + 1;
-        Vector2 objPos = new Vector2(pos.x + pW / 2.0f - (float)grid.width/ 2.0f, pos.y + pH / 2.0f - (float)grid.height/2.0f);
+        Vector2 objPos = new Vector2(pos.x + pW / 2.0f - (float)grid.width / 2.0f, pos.y + pH / 2.0f - (float)grid.height / 2.0f);
 
         //Test if obejct in in layerBounds
-        if (objPos.x  < 0 || objPos.x + grid.width > pW || objPos.y  < 0 || objPos.y + grid.height > pH) return false;
+        if (objPos.x < 0 || objPos.x + grid.width > pW || objPos.y < 0 || objPos.y + grid.height > pH) return false;
         //test if theres is an object
         for (int x = (int)objPos.x, xm = 0; x < (int)objPos.x + grid.width; x++, xm++)
         {
-            for (int y = (int)objPos.y, ym = 0; y < (int)objPos.y + grid.height; y++, ym++)
+            for (int y = (int)objPos.y, ym = grid.height-1; y < (int)objPos.y + grid.height; y++, ym--)
             {
                 //Debug.Log("Check mat at: " + x.ToString() + " " + y.ToString());                                    
                 if (grid.hitMat[xm][ym] && Editor_Grid.Instance.levelGrid[matlayer][x][y] != null)
@@ -31,22 +31,21 @@ public class CommandHelper
 
     public static void setMatrixForceOverride(int matlayer, Vector2 pos, Gridable grid, GameObject value)
     {
-        
+
         int pW = Editor_Grid.Instance.levelGrid[matlayer].Length + 1;
         int pH = Editor_Grid.Instance.levelGrid[matlayer][0].Length + 1;
         Vector2 objPos = new Vector2(pos.x + pW / 2.0f - (float)grid.width / 2.0f, pos.y + pH / 2.0f - (float)grid.height / 2.0f);
 
         for (int x = (int)objPos.x, xm = 0; x < (int)objPos.x + grid.width; x++, xm++)
         {
-            for (int y = (int)objPos.y, ym = 0; y < (int)objPos.y + grid.height; y++, ym++)
+            for (int y = (int)objPos.y, ym = grid.height-1; y < (int)objPos.y + grid.height; y++, ym--)
             {
-                //if (grid.hitMat[xm][ym])
-                //{
-
-                //Debug.Log(value);
-                //Debug.Log("mat at: " + x.ToString() + " " + y.ToString());
-                Editor_Grid.Instance.levelGrid[matlayer][x][y] = value;
-                //}
+                if (grid.hitMat[xm][ym])
+                {
+                    //Debug.Log(value);
+                    //Debug.Log("mat at: " + x.ToString() + " " + y.ToString());
+                    Editor_Grid.Instance.levelGrid[matlayer][x][y] = value;
+                }
             }
         }
     }
