77a8e2d201ed7d7fa39c95d2be5f9bad4a82f7d5
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2014-05-28 13:10:01 +0200

    pukeball looks good

--- Project/Frohdo/Assets/Scripts/Game/PukeBall.cs
+++ Project/Frohdo/Assets/Scripts/Game/PukeBall.cs
@@ -4,10 +4,20 @@ using System.Collections;
 public class PukeBall : MonoBehaviour {
 
     private Colorable colorable_;
+    private GameObject look_;
 
     void Awake()
     {
-        colorable_ = gameObject.GetComponent<Colorable>();
+        colorable_ = gameObject.GetComponentInChildren<Colorable>();
+        look_ = gameObject.GetComponentInChildren<SpriteRenderer>().gameObject;
+    }
+
+    void Update()
+    {
+        float rotation = Mathf.Atan2(rigidbody2D.velocity.y, rigidbody2D.velocity.x) * Mathf.Rad2Deg + 180.0f;
+
+        look_.transform.rotation = Quaternion.AngleAxis(rotation, new Vector3(0.0f, 0.0f, 1.0f));
+//        transform.rotation = Quaternion.LookRotation(new Vector3(0.0f, rigidbody2D.velocity.x, rigidbody2D.velocity.y));
     }
 
     void OnCollisionEnter2D(Collision2D coll)
