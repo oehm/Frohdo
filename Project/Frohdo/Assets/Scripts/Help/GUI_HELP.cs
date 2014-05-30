c084c1698e03b61517a4a3b63d7aecfec797ac41
Tobias Pretzl <tpretzl.mmt-b2012@fh-salzburg.ac.at>
2014-05-30 22:49:45 +0200

    added help menu. still pretty ugly but does its job. have to fix displaying on small resolutions

--- Project/Frohdo/Assets/Scripts/Help/GUI_HELP.cs
+++ Project/Frohdo/Assets/Scripts/Help/GUI_HELP.cs
@@ -3,13 +3,114 @@ using System.Collections;
 
 public class GUI_HELP : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private delegate void Menudelegate();
+    private Menudelegate menufunction;
+
+    public GUISkin skin;
+
+    public Texture Basics;
+    public Texture ColorMixing;
+    public Texture Controlls;
+    public Texture Editor;
+    public Texture Support;
+
+    void Awake()
+    {
+        menufunction = main;
+    }
+
+    void OnGUI()
+    {
+        menufunction();
+    }
+
+
+    private void main()
+    {
+        GUILayout.BeginArea(new Rect((ForceAspectRatio.screenWidth) / 2 - 300 + ForceAspectRatio.xOffset, (ForceAspectRatio.screenHeight) / 2 - 200 + ForceAspectRatio.yOffset, 600, 400));
+
+        if (GUILayout.Button("The Basics", skin.button))
+        {
+            menufunction = basics;
+        }
+        if (GUILayout.Button("Mixing Colors", skin.button))
+        {
+            menufunction = colors;
+        }
+        if (GUILayout.Button("Controlls", skin.button))
+        {
+            menufunction = controlls;
+        }
+        if (GUILayout.Button("Editor", skin.button))
+        {
+            menufunction = editor;
+        }
+        if (GUILayout.Button("Support and Bug report", skin.button))
+        {
+            menufunction = support;
+        }
+        if (GUILayout.Button("Back", skin.button))
+        {
+            SceneManager.Instance.loadScene(SceneManager.Scene.MainMenu);
+        }
+        GUILayout.EndArea();
+    }
+
+    private void backButton()
+    {
+        if(GUILayout.Button("Back", skin.button))
+        {
+            menufunction = main;
+        }
+    }
+
+    private void basics()
+    {
+        GUILayout.BeginArea(ForceAspectRatio.screenRect);
+
+        GUILayout.Box(Basics, skin.box);
+
+        backButton();
+        GUILayout.EndArea();        
+    }
+
+    public void colors()
+    {
+        GUILayout.BeginArea(ForceAspectRatio.screenRect);
+
+        GUILayout.Box(ColorMixing, skin.box);
+
+        backButton();
+        GUILayout.EndArea();
+    }
+
+    public void controlls()
+    {
+        GUILayout.BeginArea(ForceAspectRatio.screenRect);
+
+        GUILayout.Box(Controlls, skin.box);
+
+        backButton();
+        GUILayout.EndArea();
+    }
+
+    public void editor()
+    {
+        GUILayout.BeginArea(ForceAspectRatio.screenRect);
+
+        GUILayout.Box(Editor, skin.box);
+
+        backButton();
+        GUILayout.EndArea();
+    }
+
+    public void support()
+    {
+        GUILayout.BeginArea(ForceAspectRatio.screenRect);
+
+        GUILayout.Box(Support, skin.box);
+
+        backButton();
+        GUILayout.EndArea();
+    }
 }
